8fd07e9af8e1724095e7566d4154cc8663f03629
Dima Kovalenko <Dima Kovalenko>
2017-03-22 16:10:23 +0300

    запуск одногог шарика, функция рандома

--- game.js
+++ game.js
@@ -1,5 +1,11 @@
 'use strict';
 
+function rand(from, to){
+  return Math.floor(Math.random() * (to - from + 1)) + from;
+
+}
+
+ // показываем кнопку
 function showBubble(bubble){
   bubble.classList.add('up');
 }
@@ -13,15 +19,18 @@ function  hideButton(){
   startButton.style.display = 'none';
 }
 
+//показываем кнопку
 function showButton () {
   startButton.style.display = 'block';
 }
 const lines = document.getElementsByClassName('hole');
 const startButton = document.querySelector('.startButton');
 startButton.addEventListener('click', () => { 
-  hideButton()
-
-
+  hideButton();
+    const bubble = lines[rand(0, lines.length -1)];
+    showBubble(bubble);
+    setTimeout(() => {
+      hideBubble(bubble);
+      showButton();
+    }, 2000)
 });
-
-showBubble(lines[2]);
