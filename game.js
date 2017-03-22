d56c15b2fa01fc3c20b779ef08ac1620f049b579
Dima Kovalenko <Dima Kovalenko>
2017-03-22 17:46:25 +0300

    очки, таймер игры

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 'use strict';
+
 //рандомная функция
 function rand(from, to){
   return Math.floor(Math.random() * (to - from + 1)) + from;
@@ -43,22 +44,77 @@ function showButton () {
 
 //следующий шарик
 function nextBubble(){
+  if(!isGameStarted)
+  {
+    return resetGame();
+  }
   const bubble  = getRandomBubble();
   showBubble(bubble);
-  setTimeout(() => {
+  bubble.timeout = setTimeout(() => {
     hideBubble(bubble);
-    nextBubble();
+    if(isGameStarted){
+      nextBubble();
+    }
+    else{
+      resetGame();
+    }
   }, 2000);
 }
-
-
 
 // обработка клика по шарику
 function handleBubbleClick () {
      const bubble = this.parentElement;
+     clearTimeout(bubble.timeout);
      killBubble(bubble);
+     hideBubble(bubble);
+     nextBubble();
+     incPoints();
 }
 
+//счет очков
+function incPoints(){
+  ++currentPoints;
+  showPoints();
+}
+
+//отображение очков
+function showPoints(){
+  scoreView.dataset.points = currentPoints;
+}
+
+//
+function resetPoints(){
+  currentPoints = 0;
+  updateScoreboard();
+}
+
+//
+function updateScoreboard(){
+  scoreView.dataset.points = currentPoints;
+}
+
+//начало игры
+function startGame(){
+  resetPoints();
+  isGameStarted = true;
+  hideButton();
+  nextBubble();
+  setTimeout(stopGame, GAME_TIMEOUT);
+}
+
+//конец игры
+function stopGame(){
+  isGameStarted = false;
+  resetGame();
+}
+
+function resetGame(){
+  showButton();
+}
+
+let currentPoints = 0;
+let isGameStarted = false;
+const GAME_TIMEOUT = 15000;
 const lines = document.getElementsByClassName('hole');
 const bubbles = document.getElementsByClassName('bubble');
 const startButton = document.querySelector('.startButton');
@@ -67,7 +123,8 @@ for(let bubble of bubbles){
   bubble.addEventListener('click', handleBubbleClick);
 }
 
+const scoreView = document.getElementById('currentScoreView'); 
+
 startButton.addEventListener('click', () => { 
-  hideButton();
-  nextBubble();
+  startGame()
 });
